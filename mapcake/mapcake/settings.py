abea73414dc0229516a3f43167e22414f6917e3f
crtGis <c.rollet25@wanadoo.fr>
2013-05-29 20:28:15 +0200

    Modification du template
    Signed-off-by: crtGis <c.rollet25@wanadoo.fr>

--- mapcake/mapcake/settings.py
+++ mapcake/mapcake/settings.py
@@ -103,7 +103,7 @@ ROOT_URLCONF = 'mapcake.urls'
 WSGI_APPLICATION = 'mapcake.wsgi.application'
 
 TEMPLATE_DIRS = (
-    "./../templates"	
+    "./templates"	
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.

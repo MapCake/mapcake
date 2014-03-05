5659b8246d3c749cba36e43fa67f02ad5717543a
David Martinez Morata <damarmo@gmail.com>
2014-03-05 12:22:11 +0100

    Update local.py

--- mapcake/mapcake/settings/local.py
+++ mapcake/mapcake/settings/local.py
@@ -23,8 +23,8 @@ DATABASES = {
         'NAME': 'mapcake_project',  # Or path to database file if using
         # sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'noviembre',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',  # Empty for localhost through domain sockets or
         # '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
